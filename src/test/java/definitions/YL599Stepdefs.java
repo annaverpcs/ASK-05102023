f5473ef8d3b592d41b8a062f362c179e8ae9778f
Yuliya Lesyuk <yuliyalesyuk@Yuliyas-MBP.lan>
2023-06-23 11:11:46 -0700

    yuliya changes

---
+++ src/test/java/definitions/YL599Stepdefs.java
@@ -0,0 +1,10 @@
+package definitions;
+
+import cucumber.api.java.en.Then;
+
+public class YL599Stepdefs {
+    @Then("YL retrieve information from db for email {string}")
+    public void ylRetrieveInformationFromDbForEmail(String arg0) {
+
+    }
+}
